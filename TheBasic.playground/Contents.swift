3ae872cad1b786efe4afe79d84c27456336fd4ab
seungjin <seungjin429@naver.com>
2020-01-03 12:17:44 +0900

    Enumerations 적용

--- TheBasic.playground/Contents.swift
+++ TheBasic.playground/Contents.swift
@@ -120,8 +120,8 @@ for i in (0..<10) {
 print("\n-------------- [Function] --------------\n")
 
 /*:
-## Function Syntax
-![Function Syntax](func.png)
+ ## Function Syntax
+ ![Function Syntax](func.png)
  */
 
 func funcName(param1: Int, param2: String) -> String {
@@ -137,23 +137,23 @@ func greetAgain(person: String) -> String {
 greetAgain(person: "Seungjin")
 
 /*:
----
-## Argument Label
+ ---
+ ## Argument Label
 
-### Argument Label Declaration Syntax
+ ### Argument Label Declaration Syntax
 
-![Argument Label](arg-label.png)
+ ![Argument Label](arg-label.png)
 
-### Calling Function with Argument Label
-![Calling Function with Argument Label](arg-call.png)
-*/
+ ### Calling Function with Argument Label
+ ![Calling Function with Argument Label](arg-call.png)
+ */
 /*:
  ## Parameter
  * 매개변수 = 인자 = Parameter
  * 인수를 담는 변수의 한 종류로서 해당 함수 내부 스코프에만 영향
  * 대부분 call by value 가 기본
  * call by reference 는 호출할 때 사용한 전달인자에까지 영향
-*/
+ */
 
 
 func testFunc1(_ param: Int) {
@@ -178,15 +178,283 @@ func testFunc3() {
 /*
 
  func testFunc3() -> Void {
-   print("ㅎㅇ")
+ print("ㅎㅇ")
  }
 
  func testFunc3() -> () {
-   print("ㅎㅇ")
+ print("ㅎㅇ")
  }
 
  */
 
 testFunc3()
+
+
+print("\n-------------- [if] --------------\n")
+
+/***************************************************
+ if <#condition#> {
+ <#code#>
+ }
+
+ if <#condition#> {
+ <#statements#>
+ } else {
+ <#statements#>
+ }
+ ***************************************************/
+
+// 다른 언어처럼 ()를 붙히지 않음
+
+if age >= 30 {
+  print("true")
+} else {
+  print("false")
+}
+
+print("\n-------------- [switch] --------------\n")
+
+/***************************************************
+ switch <#value#> {
+ case <#value 1#>:
+ <#respond to value 1#>
+ case <#value 2#>,
+ <#value 3#>:
+ <#respond to value 2 or 3#>
+ default:
+ <#otherwise, do something else#>
+ }
+
+ - switch 문은 가능한 모든 사례를 반드시 다루어야 함 (Switch must be exhaustive)
+ ***************************************************/
+
+let alphabet = "a"
+
+switch alphabet {
+case "a":
+  print("a")
+case "z":
+  print("z")
+default:
+  break
+}
+
+// 범위 연산자로도 가능
+
+let height: Int = 180
+
+switch height {
+case 0...50:
+  print("0 ~ 50")
+case 51...100:
+  print("51 ~ 100")
+case 101...150:
+  print("101 ~ 150")
+default:
+  print("151이상")
+}
+
+// 다중 조건도 가능
+
+switch alphabet {
+case "a", "b", "c", "d", "e", "f":
+  print("이중에서 하나")
+case "g", "h", "i", "j", "k", "l":
+  print("걸리겠지 ?")
+default:
+  print("아니면 여기")
+}
+
+
+print("\n-------------- [guard] --------------\n")
+
+/*:
+ ## Early Exit
+
+ * guard statement
+ * guard 구문은 if구문과 유사하게 Bool 타입의 값으로 동작하는 기능.
+ * if문과는 다르게 guard문은 else가 항상 따라와야됨.
+ * guard 뒤에 실행 결과가 true일 때 코드가 계속 실행되고, false라면 else의 블록 내부 코드를 실행하게 된다.
+ * if 구문을 쓰면 예외사항을 else 블록으로 처리해야 하지만 예외사항만을 처리하고 싶다면 guard 구문을 쓰는 것이 훨씬 간편하다.
+ */
+
+/***************************************************
+ if <#condition#> {
+ <#code#>
+ }
+
+ guard <#condition#> else {
+ <#code#>
+ }
+ ***************************************************/
+
+let version = 100
+func update(by version: Int) {
+  guard version == 100 else {
+    print("업데이트 필요 없음")
+    return
+  }
+
+  print("업데이트 필요")
+}
+
+update(by: version)
+
+
+print("\n-------------- [loops] --------------\n")
+
+/*:
+ # C 스타일의 for 문은 지원하지 않음
+
+ ```
+ for (int i = 0; i <= 5; i++) {
+ C 스타일의 for 문
+ }
+ ```
+ */
+
+
+for index in 1...4 {
+  print("\(index) times 5 is \(index * 5)")
+}
+
+let list = ["Swift", "Programming", "Language"]
+for str in list {
+  print(str)
+}
+
+for chr in "Hello" {
+  print(chr, terminator: " ")
+}
+
+
+print("\n-------------- [Tuples] --------------\n")
+
+/*
+ 이름 없는 튜플
+ */
+let threeTuple: (Int, Int, Int) = (1, 2, 5)
+type(of: threeTuple)
+
+// 접근 방법
+threeTuple.0
+threeTuple.1
+threeTuple.2
+
+// 다른 타입도 가능
+let fiveTuple: (Int, String, Double, Float, Character) = (10, "10", 10.0, 10.0, "1")
+print(fiveTuple)
+
+fiveTuple.0
+fiveTuple.1
+fiveTuple.2
+fiveTuple.3
+fiveTuple.4
+
+
+/*
+ 이름 있는 튜플
+ */
+
+let twoTuple: (value1: Int, value2: Int) = (1, 2)
+twoTuple.value1
+twoTuple.value2
+
+
+print("\n-------------- [enumeration] --------------\n")
+
+enum Language1 {
+  case swift, kotlin, java, python, javascript
+}
+
+enum Language2 {
+  case swift
+  case kotlin
+  case java
+  case python
+  case javascript
+}
+
+//접근 방법
+Language1.swift
+Language1.python
+
+
+//switch와 주로 사용
+let python = Language1.javascript
+
+switch python {
+case .javascript: print("찾았다")
+default: break
+}
+
+/*:
+ ### Associated Values
+ */
+enum Barcode {
+  case upc(Int, Int, Int, Int)
+  case qrCode(String)
+}
+
+let productUPC = Barcode.upc(8, 85909, 51226, 3)
+let productQR = Barcode.qrCode("AIUPOIJQLXLKHF")
+
+// 연관값으로 값이 넘어옴
+switch productQR {
+case .upc(let numberSystem, let manufacturer, let product, let check):
+  print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).")
+case .qrCode(let productCode):
+  print("QR code: \(productCode)")
+}
+
+
+/*:
+ ### Raw Values
+ * enum에 타입을 지정할 수 있음
+ * raw value 는 해당 Enumeration 내에서 반드시 고유한 값이어야 함.
+ */
+
+// enum에 타입을 지정할 수 있음
+// Strings, Characters, or any of the Integer or Floating-point number types
+enum AppleMobile: String {
+  case iPhone8 = "8"
+  case iPhoneX = "10"
+  case iPhone11Pro = "11"
+}
+
+enum Weekday: Int {
+  case sunday, monday, tuesday, wednesday, thursday, friday, saturday
+}
+
+enum WeekdayName: String {
+  case sunday="1", monday, tuesday, wednesday, thursday, friday, saturday
+}
+
+
+// 접근 방법
+print(AppleMobile.iPhone8.rawValue)
+print(AppleMobile.iPhoneX.rawValue)
+
+
+/*
+ (rawValue: 값)으로 enum값에 값이 있는지 체크할 수 있다.
+ 즉, 값이 있는지 없는지 체크하는 로직 구성 가능
+ */
+
+let day = Weekday(rawValue: 3)
+
+switch day {
+case .sunday: print("일요일")
+case .monday: print("월요일")
+case .tuesday: print("화요일")
+case .wednesday: print("수요일")
+case .thursday: print("목요일")
+case .friday: print("금요일")
+case .saturday: print("토요일")
+default: print("None")
+}
+
+
+
 
 
