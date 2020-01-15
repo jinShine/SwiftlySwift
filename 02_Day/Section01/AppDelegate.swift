9d3db2ca190e69410a970999fb4b594c3caed3a7
seungjin <seungjin429@naver.com>
2020-01-15 15:41:53 +0900

    XCode 알아보기

---
+++ 02_Day/Section01/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Section01
+//
+//  Created by Seungjin on 09/01/2020.
+//  Copyright © 2020 jinnify. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    print("didFinishLaunchingWithOptions")
+
+    return true
+  }
+
+}
+
