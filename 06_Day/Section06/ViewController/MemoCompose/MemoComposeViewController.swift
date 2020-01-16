7cd0e62d14561f3553c7d3b5b7fbcfccd62682c3
jinnify <seungjin429@gmail.com>
2020-01-17 01:07:22 +0900

    MemoCompose UI 구현 및 Outlet, Action 연결 완료

---
+++ 06_Day/Section06/ViewController/MemoCompose/MemoComposeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MemoComposeViewController.swift
+//  Section06
+//
+//  Created by seungjin on 2020/01/17.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class MemoComposeViewController: UIViewController {
+  
+  @IBOutlet weak var contentTextView: UITextView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  @IBAction func dismiss(_ sender: UIBarButtonItem) {
+    
+  }
+  
+  @IBAction func save(_ sender: UIBarButtonItem) {
+    
+  }
+  
+}
