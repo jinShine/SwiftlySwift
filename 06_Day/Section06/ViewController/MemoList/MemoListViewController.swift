4be26329250e9703d5292597d5749e82964d13d2
jinnify <seungjin429@gmail.com>
2020-01-16 23:29:26 +0900

    MemoListViewController 생성

---
+++ 06_Day/Section06/ViewController/MemoList/MemoListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemoListViewController.swift
+//  Section06
+//
+//  Created by seungjin on 2020/01/16.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+  
+}
