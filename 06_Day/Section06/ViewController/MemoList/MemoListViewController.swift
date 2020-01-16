44ed23f74f289203598f70fad6adee1d9bc32043
jinnify <seungjin429@gmail.com>
2020-01-16 23:48:45 +0900

    테이블뷰 구현 후 필수 Datasource 메서드까지 구현

--- 06_Day/Section06/ViewController/MemoList/MemoListViewController.swift
+++ 06_Day/Section06/ViewController/MemoList/MemoListViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class MemoListViewController: UIViewController {
   
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    setupUI()
   }
+  
+  private func setupUI() {
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+}
+
+extension MemoListViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+  
+}
+
+extension MemoListViewController: UITableViewDelegate {
   
 }
