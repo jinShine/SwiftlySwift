576f34cc1e122c3539aff6d31817451f1c4420f0
jinnify <seungjin429@gmail.com>
2020-01-16 23:24:58 +0900

    폴더 구조

---
+++ 06_Day/Section06/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Section06
+//
+//  Created by seungjin on 2020/01/16.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
