1147a4dd552f2bf8672513b2fbcb9ecef2f8ee55
jinnify <seungjin429@gmail.com>
2020-01-19 20:06:47 +0900

    ContentCell, DateCell추가 후 스토리보드에 연결

---
+++ 06_Day/Section06/ViewController/MemoDetail/ContentCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContentCell.swift
+//  Section06
+//
+//  Created by seungjin on 2020/01/19.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class ContentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ 06_Day/Section06/ViewController/MemoDetail/DateCell.swift
@@ -0,0 +1,24 @@
+//
+//  DateCell.swift
+//  Section06
+//
+//  Created by seungjin on 2020/01/19.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class DateCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
