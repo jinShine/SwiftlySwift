5c89d4895059bbb6bb1e38cceff4eab9a9979da4
jinnify <seungjin429@gmail.com>
2020-01-19 19:27:39 +0900

    MemoDetailViewController 생성

---
+++ 06_Day/Section06/ViewController/MemoDetail/MemoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoDetailViewController.swift
+//  Section06
+//
+//  Created by seungjin on 2020/01/19.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class MemoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
