b7f8214831819badaf15b7b708d791fbf637c2b3
jinnify <seungjin429@gmail.com>
2020-09-28 23:51:09 +0900

    add @propertyWrapper

---
+++ 01_Day/Swift_Language.playground/Pages/Attributes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ ## propertyWrapper
+ */
+
+@propertyWrapper
+struct WhitespaceTrimmable {
+  
+  private(set) var value: String = ""
+  
+  var wrappedValue: String {
+    get { value }
+    set { value = newValue.trimmingCharacters(in: .whitespacesAndNewlines) }
+  }
+  
+  init(wrappedValue: String) {
+    self.wrappedValue = wrappedValue
+  }
+}
+
+struct Service {
+  @WhitespaceTrimmable var title: String
+  
+  init(title: String) {
+    self.title = title
+  }
+}
+
+let service = Service(title: "안녕!!!!!!!!!     ")
+print(service.title)
+
+//: [Next](@next)
