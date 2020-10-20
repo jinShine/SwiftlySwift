26dfae1c28ade439ede90d41945a5a35b93e89bb
jinShine <seungjin429@gmail.com>
2020-10-20 21:47:35 +0900

    - BaseNavigationController 생성

---
+++ 16_Day_CodingTest/H_CodingTest/Herren/Common/BaseNavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseNavigationController.swift
+//  Herren
+//
+//  Created by Buzz.Kim on 2020/10/20.
+//  Copyright © 2020 jinnify. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+  
+}
