77f948f756b8e4b2ed834328e5a72a9c97c52bf8
jinShine <seungjin429@gmail.com>
2020-11-09 23:50:37 +0900

    custom statusBar style

---
+++ Supplement/CustomNavigationBar/CustomNavigationBar/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  CustomNavigationBar
+//
+//  Created by Buzz.Kim on 2020/11/08.
+//  Copyright © 2020 jinnify. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Supplement/CustomNavigationBar/CustomNavigationBar/UINavigationController+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UINavigationController+Extension.swift
+//  CustomNavigationBar
+//
+//  Created by Buzz.Kim on 2020/11/09.
+//  Copyright © 2020 jinnify. All rights reserved.
+//
+
+import Foundation
