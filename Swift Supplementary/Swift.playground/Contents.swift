fd24828f859a6e0c0db9cc7b9d8e7e1ba2aeb577
jinShine <seungjin429@gmail.com>
2020-12-08 23:55:14 +0900

    swift 문법 요약 작성중

--- Swift Supplementary/Swift.playground/Contents.swift
+++ Swift Supplementary/Swift.playground/Contents.swift
@@ -15,31 +15,3 @@ print("숫자 \(num)")
 
 
 print("\n-------------- [Constants and Variables] --------------\n")
-
-
-
-let jin = { (name: String, age: Int) in
-  print("이름은 \(name)이고, 나이는 \(age)입니다.")
-}
-
-func personInfo(to: (String, Int) -> Void) {
-  to("김승진", 30)
-}
-
-
-personInfo(to: jin)
-
-func reduce(_ values: [Int], resultTotal: (Int, Int) -> Int) -> Int {
-  var current = values[0]
-  
-  for value in values[1...] {
-    current = resultTotal(current, value)
-  }
-  
-  return current
-}
-
-let sum = reduce([1,2,3,4,5]) { (total, value) -> Int in
-  return total + value
-}
-print(sum)
